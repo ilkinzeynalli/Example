05dc649ff8a35032283d15c5d155aa02ccf66b95
ilkin zeynalli <ilkin.zeynalli@bil.omu.edu.tr>
2021-07-06 12:55:24 +0400

    Pipe

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -3,21 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app',
   template: `
-      <input [(ngModel)]="email"  (keyup.enter)="onKeyUp()" />
-      {{email}}
+      <h3>Date pipe</h3>
+      <p>{{today | date}}</p>
+      <p>{{today | date: 'short'}}</p>
+      <p>{{today | date: 'M/d/yy h:mm a'}}</p>
+      <p>{{today | date: 'medium'}}</p>
+      <p>{{today | date: 'hh:mm:ss a'}}</p>
+
+      <h3>Decimal pipe</h3>
+      <p>{{students | number}}</p>
+      <p>{{students | number:'1.2-2'}}</p>
+
+      <h3>Lower - Upper - Tittle case</h3>
+      <p>{{title | titlecase}}</p>
+      <p>{{title | uppercase}}</p>
+      <p>{{title | lowercase}}</p>
+
+      <h3>Currency pipe</h3>
+      <p>{{price | currency}}</p>
+      <p>{{completed | percent}}</p>
+      <p>{{completed | percent : '2.1-10'}}</p>
+
   `,
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent {
 
-  email:string = "ilkin@hotmail.com";
-
   constructor() { }
 
-  onKeyUp(){
-    console.log(this.email);
-    // if($event.keyCode === 13){
-    //   console.log($event.target.value);
-    // }
-  }
+  today = new Date();
+  students = 12345;
+  title = "ilkin";
+  price = 1234;
+  completed = 0.26;
 }
