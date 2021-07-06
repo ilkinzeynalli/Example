38f2f349353ad25cfd34d0d519a89917f11fbf94
ilkin zeynalli <ilkin.zeynalli@bil.omu.edu.tr>
2021-07-06 10:32:31 +0400

    Styles

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -1,35 +1,48 @@
-import { Component } from "@angular/core";
-import { Product } from "./models/product.model";
-import { ProductRepository } from "./models/repository.model";
+import { Component } from '@angular/core';
+import { Product } from './models/product.model';
+import { ProductRepository } from './models/repository.model';
 
 @Component({
-    selector:"app",
-    templateUrl:"./product.component.html",
-    styleUrls: ["./product.component.css"]
+  selector: 'app',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
 })
-export class ProductComponent{
-    productRepository: ProductRepository;
-    product: Product;
-    disabled: boolean = true;
+export class ProductComponent {
+  productRepository: ProductRepository;
+  product: Product;
+  disabled: boolean = true;
 
-    constructor(){
-        this.productRepository = new ProductRepository();
-        this.product = this.productRepository.getProductById(1);
-    }
+  constructor() {
+    this.productRepository = new ProductRepository();
+    this.product = this.productRepository.getProductById(1);
+  }
 
-    getClasses(id:number){
-        let product = this.productRepository.getProductById(id);
+  getClasses(id: number) {
+    let product = this.productRepository.getProductById(id);
 
-        return ((product.price ?? 0) < 2000 ? "bg-primary":"bg-secondary" + " m-2 p-2")
-    }
+    return (product.price ?? 0) < 2000
+      ? 'bg-primary'
+      : 'bg-secondary' + ' m-2 p-2';
+  }
 
-    getClassesMap(id: number) :Object{
-        let product = this.productRepository.getProductById(id);
+  getClassesMap(id: number): Object {
+    let product = this.productRepository.getProductById(id);
 
-        return {
-            "bg-primary": (product.price ?? 0) <= 2000,
-            "bg-secondary": (product.price ?? 0) > 2000
-        };
-    }
+    return {
+      'bg-primary': (product.price ?? 0) <= 2000,
+      'bg-secondary': (product.price ?? 0) > 2000,
+    };
+  }
 
+  color: string = 'green';
+  fontSize: string = "50px";
+
+  getStyles(id: number): Object{
+    let product = this.productRepository.getProductById(id);
+
+    return {
+        // color: (product.price ?? 0) < 2000 ? 'green':'red',
+        fontSize: product.name=='Product 2' ? '50px':'25px'
+    };
+  }
 }
